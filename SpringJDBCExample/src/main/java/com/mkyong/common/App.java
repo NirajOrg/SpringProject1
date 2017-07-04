7bfd977552d3e074f732e22d08bf04cb7459cb35
Niraj Kumar <niraj.kumar@trantorinc.com>
2017-07-04 16:49:46 +0530

    added customer2

--- SpringJDBCExample/src/main/java/com/mkyong/common/App.java
+++ SpringJDBCExample/src/main/java/com/mkyong/common/App.java
@@ -7,7 +7,9 @@ import com.mkyong.customer.model.Customer;
 
 public class App 
 {
-	/* Main method to start */
+	/* Main method to start
+	 * adding customer 1 
+	 */
     public static void main( String[] args )
     {
     	ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
@@ -16,8 +18,15 @@ public class App
         Customer customer1 = new Customer(1, "mkyong",28);
         customerDAO.insert(customer1);
     	
+        Customer customer2 = new Customer(2, "niraj",28);
+        customerDAO.insert(customer2);
+        
         Customer customer1 = customerDAO.findByCustomerId(1);
         System.out.println(customer1);
+        
+
+        Customer customer2 = customerDAO.findByCustomerId(2);
+        System.out.println(customer2);
         
     }
 }
